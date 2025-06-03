1b0e771b5851cd1a0898e95408f7aa0b7ec0a332
rdx40 <ivanjaison@gmail.com>
2025-06-03 09:06:02 +0530

    ignore readmes and docs during push to acr

--- obj/Debug/net8.0/WeatherAPIWrapper.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherAPIWrapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPIWrapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d9c877c21b6cb3ce5667eec8688da945e652a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59eddeb6d2e4e57233063cfdabf69a5c115c661")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPIWrapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPIWrapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
